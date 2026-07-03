ce2292a3e268e9fa11a0ed04ca3757bf72d6ceed
24wh1a05g4 <24wh1a05g4@bvrithyderabad.edu.in>
2026-07-03 12:20:26 +0530

    this is frontend app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+ 
+// web server:
+ app.use(express.static("frontend"))
+
+
+app.listen(port, function(){
+  console.log("Server is running on http://localhost:"+port)
+})
+    
+
+// 'npm install express' from terminal
