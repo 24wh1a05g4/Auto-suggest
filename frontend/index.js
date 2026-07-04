49500e2432863041da58c1c46196f0b4a9e31381
24wh1a05g4 <24wh1a05g4@bvrithyderabad.edu.in>
2026-07-04 09:56:48 +0530

    image toggles

--- frontend/index.js
+++ frontend/index.js
@@ -22,4 +22,22 @@ function toggle(){
   document.getElementById("card-name").innerText=users[curUserId].name;
   document.getElementById("card-gender").innerText=users[curUserId].gender;
 }
+function getrandomUser(){
+  fetch("https://randomuser.me/api/")
+  .then(function(data){
+    return data.json();
+  })
+  
+  .then(function(parsedData){
+    let gender = parsedData.results[0].gender;
+    let first = parsedData.results[0].name.first;
+    let last = parsedData.results[0].name.last;
+    let name = first + " " + last;
+    //console.log(name);
+    let imageUrl = parsedData.results[0].picture.large;
+    document.getElementById("card-name").innerText=name;
+    document.getElementById("card-gender").innerText=gender;
+    document.getElementById("img").src=imageUrl;
+  })
+}
 console.log("Hello from js");
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
+//console.log("backend");
  
 // web server:
  app.use(express.static("frontend"))
